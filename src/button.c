b88edb304b0ae0849e1bd1fca8cad6a2b0f3e886
Ikuo Terado <eqobar@gmail.com>
2015-04-22 00:09:23 +0900

    Move code for button to other files

---
+++ src/button.c
@@ -0,0 +1,24 @@
+
+#include <stdint.h>
+#include "app_error.h"
+#include "bsp.h"
+
+#include "button.h"
+
+
+/**@brief Function for initializing the button handler module.
+ */
+void buttons_init(void)
+{
+    uint32_t err_code;
+    // Note: Before start using buttons, assign events to buttons, as shown below.
+    //      err_code = bsp_event_to_button_assign(BUTTON_0_ID, BSP_EVENT_KEY_0);
+    //      APP_ERROR_CHECK(err_code);
+    // Note: Enable buttons which you want to use.
+    //      err_code = bsp_buttons_enable((1 << WAKEUP_BUTTON_ID) | (1 << BUTTON_0_ID));
+    //      APP_ERROR_CHECK(err_code);
+    // Note: If the only use of buttons is to wake up, the bsp module can be omitted, and
+    // the wakeup button can be configured by
+    err_code = bsp_buttons_enable(1 << WAKEUP_BUTTON_ID);
+    APP_ERROR_CHECK(err_code);
+}
--- src/main.c
+++ src/main.c
@@ -44,14 +44,11 @@
 #include "bsp.h"
 
 #include "advertising.h"
+#include "button.h"
 #include "power.h"
 
 
 #define IS_SRVC_CHANGED_CHARACT_PRESENT 0                                           /**< Include or not the service_changed characteristic. if not enabled, the server's database cannot be changed for the lifetime of the device*/
-
-#define WAKEUP_BUTTON_ID                0                                           /**< Button used to wake up the application. */
-// YOUR_JOB: Define any other buttons to be used by the applications:
-// #define MY_BUTTON_ID                   1
 
 #define DEVICE_NAME                     "Nordic_Template"                           /**< Name of device. Will be included in the advertising data. */
 
@@ -468,24 +465,6 @@ static void bsp_event_handler(bsp_event_t evt)
 static void gpiote_init(void)
 {
     APP_GPIOTE_INIT(APP_GPIOTE_MAX_USERS);
-}
-
-
-/**@brief Function for initializing the button handler module.
- */
-static void buttons_init(void)
-{
-    uint32_t err_code;
-    // Note: Before start using buttons, assign events to buttons, as shown below.
-    //      err_code = bsp_event_to_button_assign(BUTTON_0_ID, BSP_EVENT_KEY_0);
-    //      APP_ERROR_CHECK(err_code);
-    // Note: Enable buttons which you want to use.
-    //      err_code = bsp_buttons_enable((1 << WAKEUP_BUTTON_ID) | (1 << BUTTON_0_ID));
-    //      APP_ERROR_CHECK(err_code);
-    // Note: If the only use of buttons is to wake up, the bsp module can be omitted, and
-    // the wakeup button can be configured by
-    err_code = bsp_buttons_enable(1 << WAKEUP_BUTTON_ID);
-    APP_ERROR_CHECK(err_code);
 }
 
 
